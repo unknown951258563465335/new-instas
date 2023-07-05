dc30bec2ac1edf2e8953077ee43c95fb203ddbee
unknown951258563465335 <89399889+unknown951258563465335@users.noreply.github.com>
2023-07-05 11:29:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000
 
 app.use(express.json())
-app.use("/",require("./Routes/LoginValidatoin"))
-app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
+app.use("/",require("./Routes/LoginValidatoin"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
